8833fe62f2cf88f2587fc8b43a9ab2643b5ba66f
seb1001010010100 <36080429+seb1001010010100@users.noreply.github.com>
2018-09-01 01:58:59 -0400

    FAIT: * Ajout design des textes et spinners, classe vue et globale, et ajout du code pour afficher le texte a partir de resource TODO: * Ajout du code pour la methode onFinishInFlate, et approfondire les contraites

---
+++ app/src/main/java/ca/cours5b5/sebastienhamel/globale/GConstantes.java
@@ -0,0 +1,17 @@
+package ca.cours5b5.sebastienhamel.globale;
+
+public class GConstantes {
+
+    final int HAUTEURMIN = 4;
+    final int HAUTEURMAX = 10;
+    final int HAUTEURDEFAUT = 6;
+    final int LARGEURMIN = 4;
+    final int LARGEURMAX = 10;
+    final int LARGEURDEFAUT = 7;
+    final int GAGNERMIN = 3;
+    final int GAGNERMAX = 4;
+    final int GAGNERDEFAUT = 4;
+
+
+
+}
---
+++ app/src/main/java/ca/cours5b5/sebastienhamel/vues/VParametres.java
@@ -0,0 +1,21 @@
+package ca.cours5b5.sebastienhamel.vues;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+
+public class VParametres extends ConstraintLayout {
+
+
+    public VParametres(Context context) {
+        super(context);
+    }
+
+    public VParametres(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VParametres(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
